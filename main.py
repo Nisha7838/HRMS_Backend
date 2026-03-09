5e8a69369f7155cd639a1727953b8c58936aebbb
Nisha7838 <kr.nisha10.20@gmail.com>
2026-03-10 00:26:24 +0530

    changes

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from routes import attendance_routes, employee_routes
 
-app = FastAPI()
+app = FastAPI(title="HRMS Backend")
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI application!"}
+app.include_router(employee_routes.router)    
 app.include_router(attendance_routes.router)
-app.include_router(employee_routes.router)
+
